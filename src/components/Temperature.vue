484d690c6c6ad2b7fd529edc4a013e29bba00862
Michael Fitzgerald <fitzichael@gmail.com>
2021-05-20 16:18:29 -0400

    dk

--- src/components/Temperature.vue
+++ src/components/Temperature.vue
@@ -10,6 +10,11 @@ export default {
     return {
       temperature: -1
     }
+  },
+  mounted: function () {
+    fetch('https://www4.des.state.nh.us/rti_data/WEIN3_TABLE.HTML')
+      .then(res => res.text())
+      .then(body => console.log(body));
   }
 }
 </script>
